028d6d8b5c2a280cc7643adc52be5ad88c2401b1
hchris1 <horn_christian@web.de>
2024-03-03 17:41:02 +0100

    Add pydantic dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="openf1",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["requests>=2.25.1"],
+    install_requires=["requests>=2.25.1", "pydantic>=2.6.3"],
 )
