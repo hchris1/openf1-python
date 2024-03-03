8d3f3c26eacebdf3c592183ac910c941832c4cb8
Christian Horn <34690225+hchris1@users.noreply.github.com>
2024-03-03 20:57:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,6 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=["requests>=2.25.1", "pydantic>=2.6.3"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
 )
